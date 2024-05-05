9d87e4c709f595dd4fe85a43f910051d043ac262
Labeebct <ctlabeebthaliyil@gmail.com>
2024-05-05 15:26:12 +0530

    events has been shown in users section

--- src/components/user/events/EventsContent.jsx
+++ src/components/user/events/EventsContent.jsx
@@ -1,17 +1,44 @@
-import EventsFrame from "./EventsFrame"
+import { useEffect } from "react";
+import EventsFrame from "./EventsFrame";
+import axiosInstance from "../../../instance/axiosInstance";
+import { useState } from "react";
 
 const EventsContent = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await axiosInstance.get("/events");
+        const { data, status } = response;
+
+        if (status == 200) {
+          setEvents(data.events);
+        }
+      } catch (error) {
+        if (error.response) {
+          const { status } = error.response;
+          if (status == 500) {
+            Navigate("/500");
+          }
+        } else {
+          Navigate("/500");
+        }
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
     <div className="h-auto w-full min-h-[calc(100vh-4.5rem)]">
-        <div className="w-full grid grid-cols-1 h-auto p-5 mt-10 sm:mt-0 px-6 gap-x-6  lg:grid-cols-3 sm:grid-cols-2">
-        <EventsFrame />
-        <EventsFrame />
-        <EventsFrame />
-        <EventsFrame />
-        <EventsFrame />
-        </div>
+      <div className="w-full grid grid-cols-1 h-auto p-5 mt-10 sm:mt-0 px-6 gap-x-6  lg:grid-cols-3 sm:grid-cols-2">
+        {events.map((data) => (
+          <EventsFrame key={data._id} data={data} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EventsContent
+export default EventsContent;
--- src/components/user/events/EventsFrame.jsx
+++ src/components/user/events/EventsFrame.jsx
@@ -1,22 +1,30 @@
-import weddingImg from "../../../assets/bg (7).jpg";
-
-const EventsFrame = () => {
+const EventsFrame = ({ data }) => {
   return (
     <div className="mb-6 shadow-box flex-1 h-auto">
       <img
-        src={weddingImg}
+        src={`http://localhost:8082/${data.eventImage}`}
         className="w-full p-6 h-[280px] object-cover rounded-md"
       />
       <div class="flex justify-between p-6 items-center">
         <div class="flex w-full flex-col gap-2">
-          <div class="text-[1.3rem] pl-1 font-bold font-inter my-1">Venue Name</div>
-          <div class="text-[.9rem] pl-1 font-scope-one font-bold text-gray-700">Category: Wedding</div>
-          <div class="text-[.9rem] pl-1 font-scope-one font-bold text-gray-700">Price: 1,20,000 / day</div>
-          <div class="text-[.9rem] pl-1 font-scope-one font-bold text-gray-700">Capacity: 200</div>
-          <div class="text-[.9rem] pl-1 font-scope-one font-bold text-gray-700">Location: City, Country</div>
-        <button className="bg-cusOrange my-2 text-[.8rem] sm:text-[.9rem] md:text-[1rem] font-inter ease-in-out duration-200 active:scale-[.97] w-full filter text-white shadow-box py-3">
-          Check Availability
-        </button>
+          <div class="text-[1.3rem] pl-1 font-bold font-inter my-1">
+            {data.venueName}
+          </div>
+          <div class="text-[.9rem] pl-1 font-roboto capitalize text-gray-700">
+            Category: {data.category}
+          </div>
+          <div class="text-[.9rem] pl-1 font-roboto capitalize text-gray-700">
+            Price: {data.price} / day
+          </div>
+          <div class="text-[.9rem] pl-1 font-roboto capitalize text-gray-700">
+            Capacity: {data.capacity} No
+          </div>
+          <div class="text-[.9rem] pl-1 font-roboto capitalize text-gray-700">
+            Location: {data.country},{data.state},{data.city}{" "}
+          </div>
+          <button className="bg-cusOrange my-2 text-[.8rem] sm:text-[.9rem] md:text-[1rem] font-inter ease-in-out duration-200 active:scale-[.97] w-full filter text-white shadow-box py-3">
+            Check Availability
+          </button>
         </div>
       </div>
     </div>
